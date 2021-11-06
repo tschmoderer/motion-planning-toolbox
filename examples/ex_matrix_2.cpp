3c12921cabe8b78141aed151f2d2558f230e1645
TS <timothee.schmoderer@insa-rouen.fr>
2021-11-06 18:40:32 +0100

    update square matrix example

--- examples/ex_matrix_2.cpp
+++ examples/ex_matrix_2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <typeinfo>
 #include "squareMatrix.h"
 
 int main(int argc, char * argv[]) {
-
 
     SquareMatrix M1 = SquareMatrix::eye(5);
     std::cout << M1 << std::endl; 
@@ -13,10 +11,6 @@ int main(int argc, char * argv[]) {
     assert(M2 == M1);
     M2 *= 2;
     std::cout << M2 << std::endl; 
-    std::cout << typeid(M2).name() << std::endl;
-
-    SquareMatrix M3 = Matrix::rand(5,5); 
-    std::cout << M3 << std::endl; 
 
     return 0;
 }
