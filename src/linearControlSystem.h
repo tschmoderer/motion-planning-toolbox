c1dc11215fa7d97eb92cbff3a9a74f8168b77e9e
TS <timothee.schmoderer@insa-rouen.fr>
2021-11-06 18:53:38 +0100

    add basic definition of control systems

---
+++ src/linearControlSystem.h
@@ -0,0 +1,13 @@
+#ifndef LINEAR_CONTROL_SYSTEM_H
+#define LINEAR_CONTROL_SYSTEM_H
+
+#include "controlSystem.h"
+
+class LinearControlSystem : ControlSystem {
+    public: 
+
+    private: 
+
+};
+
+#endif
