0b91e7be8e7961bb72820b1e4a5e53efe34d8a03
TS <timothee.schmoderer@insa-rouen.fr>
2021-11-18 01:07:50 +0100

    add test squareMatrix

---
+++ tests/test_squarematrix.cpp
@@ -0,0 +1,28 @@
+#include "squareMatrix.h"
+#include "error_code.h"
+
+void test_contructors() {
+    SquareMatrix M0; 
+    SquareMatrix M1(2); 
+    SquareMatrix * M2 =  new SquareMatrix(12); 
+    delete M2;    
+}
+
+void test_accessors() {
+}
+
+void test_operators() {
+
+}
+
+void test_methods() {
+}
+
+int main(int argc, char * argv[]) {
+    test_contructors(); 
+    test_accessors();
+    test_operators(); 
+    test_methods(); 
+
+    return 0; 
+}
