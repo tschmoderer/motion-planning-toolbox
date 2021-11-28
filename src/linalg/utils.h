30cc13bf740fb1f76202e98ef7a38acfa318e16b
TS <timothee.schmoderer@insa-rouen.fr>
2021-11-28 19:29:44 +0100

    update doc  of utils linalg

--- src/linalg/utils.h
+++ src/linalg/utils.h
@@ -4,13 +4,13 @@
 #include <cstdint>
 
 /**
-* @brief 
-* 
-* @param n 
-* @return uint32_t 
+* @brief Compute $n$-th triangular number $T_n$ defined by 
+* $$
+* T_n = \frac{n(n+1)}{2}.
+* $$
+* @param n A nonnegative integer
+* @return uint32_t $T_n$, $n$-th triangular number.
 */
 inline uint32_t triangular(uint16_t n) {return n*(n+1)/2;}
-
-void zeros_data(double * , uint32_t );
 
 #endif
