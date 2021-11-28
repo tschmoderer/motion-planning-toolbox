d9ef57273177091da5d520562f22182aac92d5a1
TS <timothee.schmoderer@insa-rouen.fr>
2021-11-28 19:11:21 +0100

    create linalg lib main file

---
+++ src/linalg/linalg.cpp
@@ -0,0 +1,22 @@
+#include "linalg.h"
+
+Vector solve(const DiagonalMatrix & D, const Vector & b) {
+    assert(D.get_dim() == b.get_dim()); 
+    Vector res(b.get_dim()); 
+    for (int i = 0; i < b.get_dim(); i++) {
+        res(i) = b(i) / D(i); 
+    }
+    return res; 
+}
+/*
+Vector solve(const LowerTriangularMatrix & L, const Vector & b) {
+    Vector res(b.get_dim()); 
+    for (int i = 0; i < b.get_dim(); i++) {
+        res(i) = b(i); 
+        for (int j = 0; j < i; j++) {
+            res(i) -= L(i,j) * res(j);
+        }
+        res(i) /= L(i,i); 
+    }
+    return res; 
+}*/
---
+++ src/linalg/linalg.h
@@ -0,0 +1,21 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+/* INCLUDE ALL HEADERS */
+#include "utils.h"
+#include "vector.h"
+#include "matrix.h"
+#include "squareMatrix.h"
+#include "lowerTriangularMatrix.h"
+
+/* ADDITIONNAL TYPES */
+typedef Vector DiagonalMatrix; 
+
+/* ADDITIONNAL METHODS */
+double tr(const DiagonalMatrix & );
+double det(const DiagonalMatrix & );
+
+Vector solve(const DiagonalMatrix & , const Vector & ); 
+Vector solve(const LTMatrix & , const Vector & ); 
+
+#endif
