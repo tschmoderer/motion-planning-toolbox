b7e176c2c0fc6ed1384e867040c6068433ce905e
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-09-13 16:14:54 +0200

    update macro version interpol and linalg lib

--- src/interpol/interpol.h
+++ src/interpol/interpol.h
@@ -11,6 +11,13 @@
 #ifndef INTERPOL_LIB_H
 #define INTERPOL_LIB_H
 
+/* VERSION MACROS */
+#define VERSION_INTERPOL_LIB       "1.0.0"
+#define AUTHOR_INTERPOL_LIB        "T. SCHMODERER/BOHEME"
+#define CONTACT_INTERPOL_LIB       "iathena@mailo.com"
+#define DOCUMENTATION_INTERPOL_LIB "https://tschmoderer.github.io/motion-planning-toolbox/html/index.html"
+
+/* INCLUDE ALL HEADERS */
 #include "interpolator.h"
 #include "interpolator1d.h"
 
--- src/linalg/linalg.h
+++ src/linalg/linalg.h
@@ -1,8 +1,22 @@
+/**
+ * @file linalg.h
+ * @author T. Schmoderer (iathena@mailo.com)
+ * @brief Main Header file for Linear Algebra Library
+ * @version 1.0
+ * @date 2022-09-13
+ * @copyright Copyright (c) 2022
+ */
+
 #ifndef LINALG_H
 #define LINALG_H
 
-/* INCLUDE ALL HEADERS */
+/* VERSION MACROS */
+#define VERSION_LINALG_LIB       "1.0.0"
+#define AUTHOR_LINALG_LIB        "T. SCHMODERER/BOHEME"
+#define CONTACT_LINALG_LIB       "iathena@mailo.com"
+#define DOCUMENTATION_LINALG_LIB "https://tschmoderer.github.io/motion-planning-toolbox/html/index.html"
 
+/* INCLUDE ALL HEADERS */
 #include "utils.h"
 #include "vector.h"
 #include "matrix.h"
@@ -14,7 +28,6 @@
 
 
 /* ADDITIONNAL METHODS */
-
 Vector solve(const DiagonalMatrix & , const Vector & );
 Vector solve(const LTMatrix & , const Vector & );
 
