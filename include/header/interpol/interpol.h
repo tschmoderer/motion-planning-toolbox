c737c90b9a418e33d97aafdaee72d1996f20bc34
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-14 11:15:04 +0200

    add interpol library

---
+++ include/header/interpol/interpol.h
@@ -0,0 +1,24 @@
+/**
+* @file interpol.h
+* @author T. Schmoderer (iathena@mailo.com)
+* @version 0.0.1
+* @date 2022-10-14
+* @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
+*/
+/**
+ * @brief
+*/
+#ifndef INTERPOL_LIB_H
+#define INTERPOL_LIB_H
+
+/* VERSION MACROS */
+#define VERSION_INTERPOL_LIB       "0.0.1"
+#define AUTHOR_INTERPOL_LIB        "T. Schmoderer"
+#define CONTACT_INTERPOL_LIB       "iathena@mailo.com"
+#define DOCUMENTATION_INTERPOL_LIB "https://tschmoderer.github.io/motion-planning-toolbox/html/index.html"
+#define CODE_INTERPOL_LIB          "https://github.com/tschmoderer/motion-planning-toolbox"
+
+/* INCLUDE HEADERS */
+#include "interpolator1d.h"
+
+#endif
---
+++ include/header/interpol/interpolator1d.h
@@ -0,0 +1,69 @@
+/**
+* @file interpolator1d.h
+* @author T. Schmoderer (iathena@mailo.com)
+* @version 0.0.1
+* @date 2022-10-14
+* @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
+*/
+/**
+ * @brief
+*/
+
+#ifndef INTERPOLATOR_1D_H
+#define INTERPOLATOR_1D_H
+
+#include "interpolator.h"
+
+using Eigen::VectorXd;
+
+/**
+ * @brief Enumeration for the different types of extensions possible 
+ * When interpolated tume is outside of the prescribed times interval then 
+ * either we return 0
+ * or we return the same value as the data at the nearest border of the nterval
+ */
+enum extend_t : uint8_t {
+    EXTEND_ZERO,
+    EXTEND_CONSTANT 
+};
+
+/**
+ * @class Interpolator1D
+ * @brief Class describing a 1D interpolation operator 
+ * etend from the general Interpolator class
+ */
+class Interpolator1D : public Interpolator {
+    public: 
+        /* CONSTRUCTORS */
+        Interpolator1D(); 
+        Interpolator1D(interpolation_method_t , extend_t , extend_t);
+
+        /* DESTRUCTOR */
+        ~Interpolator1D();
+
+        /* ACCESSORS */
+        void set_exleft(extend_t ); 
+        void set_exright(extend_t );
+
+        extend_t get_exleft() const;
+        extend_t get_exright() const;
+
+        /* METHODS */
+        double interp1d(VectorXd * , VectorXd * , double ) const;
+
+    private: 
+        /* ATTRIBUTES */
+        extend_t extend_left_method;  /*!< Method for interpolating left from the interval of prescribed times. */
+        extend_t extend_right_method; /*!< Method for interpolating right from the interval of prescribed times. */
+
+        /* METHODS */
+        double interp1d_linear(VectorXd * , VectorXd * , double , uint16_t ) const;
+        double interp1d_nearest(VectorXd * , VectorXd * , double , uint16_t ) const;
+        double interp1d_constant_left(VectorXd * , VectorXd * , double , uint16_t ) const;
+        double interp1d_constant_right(VectorXd * , VectorXd * , double , uint16_t ) const;
+
+        double interp1D_extend_left(VectorXd * , VectorXd *, double ) const;
+        double interp1D_extend_right(VectorXd * , VectorXd *, double ) const;
+};
+
+#endif
