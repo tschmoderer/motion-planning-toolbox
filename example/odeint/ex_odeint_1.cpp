ad90ecaafc03b2cc93a8f601a67176f43b035777
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-17 11:55:07 +0200

    add odeint lib

---
+++ example/odeint/ex_odeint_1.cpp
@@ -0,0 +1,47 @@
+#include "../../include/header/odeint/odeint.h"
+#include "matplotlibcpp.h"
+#include <iostream>
+
+using namespace std;
+namespace plt = matplotlibcpp;
+
+// Example Lorentz system 
+
+const double sigma = 10.0;
+const double R = 28.0;
+const double b = 8.0 / 3.0;
+
+VectorXd dyn(double t, VectorXd x) {
+    VectorXd dxdt(x.size());
+    dxdt(0) = sigma * ( x(1) - x(0) );
+    dxdt(1) = R * x(0) - x(1) - x(0) * x(2);
+    dxdt(2) = -b * x(2) + x(0) * x(1);
+
+    return dxdt;
+}
+
+int main(int argc, char * argv[]) {
+    VectorXd x0(3); 
+    ODEInt odeint(RK4); 
+    MatrixXd res;
+    int N = 2500;
+    x0 << 10.0 , 1.0 , 1.0; 
+    res = odeint.ode_int(0., 25., N, x0, dyn);
+    
+    for (int i = 0; i < N; i++) {
+        cout << res(i, 0) << '\t' << res(i, 1) << '\t' << res(i, 2) << '\t' << res(i, 3) << endl;
+    }
+
+    std::vector<double> XX, YY, ZZ; 
+    for (int i = 0; i < N; i++) {
+        XX.push_back(res(i, 1)); 
+        YY.push_back(res(i, 2)); 
+        ZZ.push_back(res(i, 3)); 
+    }
+    
+    plt::scatter(XX, YY, ZZ);
+    plt::title("Integration Lorentz system");
+    plt::show();
+    
+    return 0;
+}
---
+++ src/odeint/odeint.cpp
@@ -0,0 +1,83 @@
+#include "../../include/header/odeint/odeint.h"
+
+/* CONSTRUCTORS */
+ODEInt::ODEInt(odeint_method_t odeintmeth) {
+    this->ode_meth = odeintmeth;
+}
+
+/* DESTRCTORS */
+ODEInt::~ODEInt() {}
+
+/* METHODS */
+MatrixXd ODEInt::ode_int(double t0, double t1, int N, VectorXd x0, std::function<VectorXd(double,VectorXd)> F) {
+    VectorXd tspan = VectorXd::LinSpaced(N, t0, t1); 
+    switch (this->ode_meth) {
+    case RK4:
+        return this->rk4(tspan, x0, F);
+        break;
+    
+    default: // rk1 EULER EXPLICIT
+        return this->rk1(tspan, x0, F);
+        break;
+    }
+} 
+
+
+/* PRIVATE METHODS */
+MatrixXd ODEInt::rk1(VectorXd tspan, VectorXd x0, std::function<VectorXd(double,VectorXd)> F) {
+    MatrixXd res(tspan.size(), x0.size()+1); 
+    VectorXd state(x0.size()); 
+    VectorXd new_state(x0.size()); 
+    double dt = 0.; 
+
+    res(0, 0) = tspan(0); 
+    for (int j = 0; j < x0.size(); j++) {
+        res(0, j+1) = x0(j);
+    }
+
+    for (int i = 1; i < tspan.size(); i++) {
+        res(i, 0) = tspan(i);
+        dt = tspan(i) - tspan(i-1); 
+        state     = res(i-1, Eigen::seq(1, Eigen::last));
+        new_state = state + dt * F(tspan(i), state);
+
+        for (int j = 0; j < x0.size(); j++) {
+            res(i, j+1) = new_state(j);
+        }
+    }
+
+    return res; 
+}
+
+MatrixXd ODEInt::rk4(VectorXd tspan, VectorXd x0, std::function<VectorXd(double,VectorXd)> F) {
+    MatrixXd res(tspan.size(), x0.size()+1); 
+    VectorXd state(x0.size()); 
+    VectorXd new_state(x0.size()); 
+    VectorXd k1(x0.size()), k2(x0.size()), k3(x0.size()), k4(x0.size());
+    double dt = 0.; 
+
+    res(0, 0) = tspan(0); 
+    for (int j = 0; j < x0.size(); j++) {
+        res(0, j+1) = x0(j);
+    }
+
+    for (int i = 1; i < tspan.size(); i++) {
+        res(i, 0) = tspan(i);
+        dt = tspan(i) - tspan(i-1); 
+        state     = res(i-1, Eigen::seq(1, Eigen::last));
+
+        k1 = F(tspan(i), state);
+        k2 = F(tspan(i) + dt/2., state + k1*dt/2.);
+        k3 = F(tspan(i) + dt/2., state + k2*dt/2.);
+        k4 = F(tspan(i) + dt, state + dt*k3);
+
+        new_state = state + dt * (k1 + 2*k2 +2*k3 + k4) / 6.;
+
+        for (int j = 0; j < x0.size(); j++) {
+            res(i, j+1) = new_state(j);
+        }
+    }
+
+
+    return res;
+}
