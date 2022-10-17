82776680ab69f33a1a7fc431501370ecff4021a5
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-17 14:49:11 +0200

    add logo

---
+++ docs/img/icons/favicon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+name=rocket
+names=($name $name-white $name-bw)
+sizes=(16 32 64 128 256 512)
+
+for n in ${names[@]}; do
+    svg=$n.svg
+    res=
+    
+    for i in ${sizes[@]}; do
+        folder=$i"x"$i
+        mkdir -p $folder
+        res+=" $folder/$n.png"
+        inkscape $svg -o "$folder/$n.png" -w $i -h $i 
+    done
+
+    convert $res $name.ico
+    identify $name.ico
+done
