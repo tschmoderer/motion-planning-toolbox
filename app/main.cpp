86a2d52b041a9d55227164a8ee4f91b73eed4ce5
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-20 11:36:14 +0200

    update app

--- app/main.cpp
+++ app/main.cpp
@@ -14,61 +14,86 @@ VectorXd f_dubin_car(double t, const VectorXd & x, const Controls & u) {
 
     cntr = u(t); 
 
-    dxdt[0] = cos(x[2])*cntr(0);
-    dxdt[1] = sin(x[2])*cntr(0);
+    dxdt[0] = cos(x(2))*cntr(0);
+    dxdt[1] = sin(x(2))*cntr(0);
     dxdt[2] = cntr(1);
 
     return dxdt; 
 }
 
-/*
-Matrix dfdx(double t, const State & x, const Control & u) {
-    Matrix df_dx(STATE_DIM, STATE_DIM);
+MatrixXd dfdx(double t, const VectorXd & x, const Controls & u) {
+    MatrixXd df_dx(STATE_DIM, STATE_DIM);
+    VectorXd cntr(NB_CNTRL);
+
+    df_dx = MatrixXd::Zero(STATE_DIM, STATE_DIM);
+    cntr = u(t); 
+
+    df_dx(0, 2) = -sin(x(2))*cntr(0);
+    df_dx(1, 2) =  cos(x(2))*cntr(0);
+    
     return df_dx;
 }
 
-Matrix dfdu(double t, const State & x, const Control & u) {
-    Matrix df_du(STATE_DIM, NB_CNTRL);
+MatrixXd dfdu(double t, const VectorXd & x, const Controls & u) {
+    MatrixXd df_du(STATE_DIM, NB_CNTRL);
+    df_du = MatrixXd::Zero(STATE_DIM, NB_CNTRL);
+
+    df_du(0,0) = sin(x(2));
+    df_du(1,0) = cos(x(2));
+    df_du(2,2) = 1.;
+
     return df_du;
 }
-*/
 
 int main(int argc, char **argv) {
     hello_world_control_tlbx();
 
     // Put your code here
+    // Parameters
     constexpr uint16_t N = STATE_DIM; 
     constexpr uint16_t M = NB_CNTRL; 
     constexpr uint16_t H = 17;
     
-    constexpr double t0 = 0; 
-    constexpr double t1 = 1;
+    // Caracteristics
+    constexpr double t0  = 0; 
+    constexpr double t1  = 1;
     constexpr uint16_t T = 129;
-    VectorXd x0(N); x0 << 0., 0., 0.;
+    VectorXd x0(N); x0 << 1., 1., M_PI / 4.;
 
-    Interpolator1D * interpol_cntrl = new Interpolator1D(INTERP_LINEAR, EXTEND_ZERO, EXTEND_ZERO); 
-    Integrator1D *   integr_cntrl   = new Integrator1D(H);
-    ODEInt *         odeintgrator   = new ODEInt(RK4, T);                 
-
+    // Objects
+    ControlSystem sys(N, M, t0, t1, x0);
     Controls u(M, t0, t1, H); 
+    Interpolator1D   * interpol_cntrl = new Interpolator1D(INTERP_LINEAR, EXTEND_ZERO, EXTEND_ZERO); 
+    Integrator1D     * integr_cntrl   = new Integrator1D(H);
+    ControlSystemInt * integrator     = new ControlSystemInt(RK4, T);                 
+
+    // Settings
     u.set_interpolation_method(interpol_cntrl);
     u.set_integration_method(integr_cntrl);
 
-    cout << u << endl; 
-    cout << u.norm2() << endl;
-    cout << u(0.4) << endl << endl; 
+    u.set_cntrl(0, 1); 
+    u.set_cntrl(1, 1);
 
-    ControlSystem sys(N, M, t0, t1, x0);
     sys.set_controls(&u); 
-    sys.set_ode_integrator(odeintgrator); 
+    sys.set_cntrl_system_integrator(integrator); 
     sys.set_dynamics(f_dubin_car);
+    sys.set_dynamics_derivative_x(dfdx); 
+    sys.set_dynamics_derivative_u(dfdu);
 
-    cout << "Test dynamics" << endl; 
-    cout << sys.get_dynamics(t0, x0) << endl << endl;
+    // Control System motion planing part of the program
+    constexpr double lambda = 1e-3;
+    VectorXd XX1(N); XX1 << 1., 1., 0.;
 
+    VectorXd XX0 = sys.inout();
+    cout << "Error norm on first shot: " << (XX1-XX0).norm() << endl;
+
+    // Define path 
+    Path pi(XX0, XX1); 
+
+    // Clean memory
     delete interpol_cntrl;
     delete integr_cntrl;
-    delete odeintgrator;
+    delete integrator;
 
     // End code
     return 0;
