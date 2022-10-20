465d569bd8e91c1a9e87cdb778bc13d3f8b58623
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-10-20 17:49:48 +0200

    update docs settings

---
+++ docs/assets/js/custom.js
@@ -0,0 +1,33 @@
+function changeLogoLightDarkMode () {
+    const el = document.getElementsByTagName("doxygen-awesome-dark-mode-toggle");
+  
+    if (el.length) {
+      // Do something with el
+        console.log("element lightdark found")
+        el[0].addEventListener("click", function() {
+            if (DoxygenAwesomeDarkModeToggle.userPreference) {
+                // if dark mode is enabled
+                // then set logo to white version
+                document.getElementById("motion-planning-toolbox-logo-img").setAttribute('src', 'https://raw.githubusercontent.com/tschmoderer/motion-planning-toolbox/main/docs/img/icons/128x128/rocket-white.png')
+                document.getElementById("projectlogo").childNodes[0].setAttribute('src', 'rocket-white.png')
+            } else {
+                document.getElementById("motion-planning-toolbox-logo-img").setAttribute('src', 'https://raw.githubusercontent.com/tschmoderer/motion-planning-toolbox/main/docs/img/icons/128x128/rocket.png')
+                document.getElementById("projectlogo").childNodes[0].setAttribute('src', 'rocket.png')
+            }
+        })
+    } else {
+        console.log("retry to find elem")
+        setTimeout(changeLogoLightDarkMode, 300); // try again in 300 milliseconds
+    }
+}
+
+$(document).ready(function() {
+    if (DoxygenAwesomeDarkModeToggle.userPreference) {
+        // if dark mode is already enabled
+        // then set logo to white version
+        document.getElementById("motion-planning-toolbox-logo-img").setAttribute('src', 'https://raw.githubusercontent.com/tschmoderer/motion-planning-toolbox/main/docs/img/icons/128x128/rocket-white.png')
+        document.getElementById("projectlogo").childNodes[0].setAttribute('src', 'rocket-white.png')
+    }
+    // add event listener to change of visibility
+    changeLogoLightDarkMode();
+});  
