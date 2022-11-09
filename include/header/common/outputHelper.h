cea6748aaf0e9def59ee5c6c3f95b5a18637ae97
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-11-09 14:15:18 +0100

    update common

--- include/header/common/outputHelper.h
+++ include/header/common/outputHelper.h
@@ -2,7 +2,7 @@
 * @file outputHelper.h
 * @author T. Schmoderer (iathena@mailo.com)
 * @version 0.0.3
-* @date 2022-10-21
+* @date 2022-11-09
 * @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
 */
 /**
@@ -16,8 +16,31 @@
 #include "../controller/controls.h"
 #include "../types/trajectory.h"
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem> 
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
+
+/* NAMESPACES */
+using namespace std;
+namespace fs = std::filesystem;
+
+/* COMMON FORMAT */
+const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ",", "\n");
+
+/* CUSTOM TYPE */
+enum plot_type_t : uint8_t {
+    phase, 
+    coordinate
+};
+
+enum out_t : uint8_t {
+    trajectory, 
+    control
+};
 
 /**
  * @brief A class to standartize the output format of 
@@ -28,22 +51,39 @@ using Eigen::MatrixXd;
 class OutputHelper {
     public: 
         /* CONSTRUCTORS */
-        OutputHelper(const Controls & ); 
-        OutputHelper(const Trajectory & );
+        OutputHelper(const Controls & , string ); 
+        OutputHelper(const Trajectory & , string );
 
         /* DESTRUCTOR */
         ~OutputHelper();
 
         /* ACCESSORS */
-        void format(Eigen::IOFormat ); 
+        OutputHelper format(Eigen::IOFormat ); 
 
+        /* METHODS */
+        void save(int ) const;
+        
         /* OPERATORS */
         friend std::ostream & operator<<(std::ostream & , const OutputHelper & );
 
     private:
+        /* ATTRIBUTES */
         Eigen::IOFormat fmt = Eigen::IOFormat();
         VectorXd time_;
         MatrixXd data_;
+
+        out_t type_t; 
+
+        string out_dir_;
+        string out_file_;
+        string filename_;
+        string fileext_;
+
+        /* PRIVATE METHOD */
+        MatrixXd make_data() const; 
+        void set_output_structure(const string );
+        void create_output_dir() const; 
+        string exp_filename(int ) const;
 };
 
 #endif
--- src/common/outputHelper.cpp
+++ src/common/outputHelper.cpp
@@ -1,31 +1,95 @@
 #include "../../include/header/common/outputHelper.h"
 
-OutputHelper::OutputHelper(const Controls & c) {
+/**
+ * @brief Construct a new Output Helper:: Output Helper object
+ * 
+ * @param c 
+ * @param out output file string format can be : 
+ * "file.ext"
+ * "../file.ext"
+ * "./relative/path/file.ext"
+ * "/absolute/path/file.ext"
+ * 
+ * @warning If necessary the OutputHelper object will create the subdirectories
+ * @warning Notice that relative path are relative with respect to where you launch the program 
+ * and not where the exe is located
+ */
+OutputHelper::OutputHelper(const Controls & c, string out="./control.dat") {
     this->time_ = c.get_time();
     this->data_ = c.get_data();
+    this->type_t = control;
+
+    this->set_output_structure(out); 
+    this->create_output_dir();
 }
-OutputHelper::OutputHelper(const Trajectory & t) {
+
+OutputHelper::OutputHelper(const Trajectory & t, string out="./trajectory.dat") {
     this->time_ = t.get_time();
     this->data_ = t.get_data();
+    this->type_t = trajectory;
+
+    this->set_output_structure(out); 
+    this->create_output_dir();
 }
 
 /* DESTRUCTOR */
 OutputHelper::~OutputHelper() {}
 
 /* ACCESSORS */
-void OutputHelper::format(Eigen::IOFormat iof) {
+OutputHelper OutputHelper::format(Eigen::IOFormat iof) {
     this->fmt = iof;
+    return *this;
+}
+
+/* METHODS */
+void OutputHelper::save(int exp_nb) const {
+    // a modified save function where a number is added before the extension in the filename
+    ofstream myfile;
+    string fname = this->exp_filename(exp_nb);
+    myfile.open(fname);
+    myfile << this->make_data().format(this->fmt);
+    myfile.close();
+
+    cout << "Data successfully saved in " << fname << endl;
 }
 
 /* OPERATORS */
-
 std::ostream & operator<<(std::ostream & os, const OutputHelper & oh) {
     assert(oh.time_.size() == oh.data_.rows());
-    MatrixXd dat(oh.time_.size(), oh.data_.cols()+1); 
-    dat(Eigen::all, 0) = oh.time_;
-    dat(Eigen::all, Eigen::seq(1, Eigen::last)) = oh.data_;
 
-    os << dat.format(oh.fmt);
+    os << oh.make_data().format(oh.fmt);
     
     return os;
+}
+
+MatrixXd OutputHelper::make_data() const {
+    MatrixXd dat(this->time_.size(), this->data_.cols()+1); 
+
+    dat(Eigen::all, 0) = this->time_;
+    dat(Eigen::all, Eigen::seq(1, Eigen::last)) = this->data_;
+
+    return dat;
+}
+
+void OutputHelper::set_output_structure(string out) {
+    this->out_file_ = out; 
+    size_t pos = out.find_last_of('/');
+    this->out_dir_ = out.substr(0, pos);
+
+    this->filename_ = out.substr(pos);
+    pos = this->filename_.find_last_of('.');
+    this->fileext_ = this->filename_.substr(pos+1);
+    this->filename_ = this->filename_.substr(1,pos-1);
+}
+
+void OutputHelper::create_output_dir() const {
+    fs::create_directories(this->out_dir_);
+}
+
+string OutputHelper::exp_filename(int exp_nb) const {
+    string res; 
+    res = this->out_dir_ + "/" + this->filename_ + "-"; 
+    res += to_string(exp_nb);
+    res += "." + this->fileext_;
+    return res; 
 }
