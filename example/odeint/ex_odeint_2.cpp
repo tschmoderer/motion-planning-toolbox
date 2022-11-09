5f252e1f2e04719305da0c0de7aee0081dc355ea
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-11-09 14:14:11 +0100

    update odeint examples

--- example/odeint/ex_odeint_2.cpp
+++ example/odeint/ex_odeint_2.cpp
@@ -5,42 +5,37 @@
 using namespace std;
 namespace plt = matplotlibcpp;
 
-// Example Lorentz system 
-
-const double sigma = 10.0;
-const double R = 28.0;
-const double b = 8.0 / 3.0;
+// Example van der Pol equation
+const double mu = 1;
 
 VectorXd dyn(double t, VectorXd x) {
     VectorXd dxdt(x.size());
-    dxdt(0) = sigma * ( x(1) - x(0) );
-    dxdt(1) = R * x(0) - x(1) - x(0) * x(2);
-    dxdt(2) = -b * x(2) + x(0) * x(1);
+    dxdt(0) = x(1);
+    dxdt(1) =mu*(1-x(0)*x(0))*x(1)-x(0);
 
     return dxdt;
 }
 
 int main(int argc, char * argv[]) {
-    VectorXd x0(3); x0 << 10.0 , 1.0 , 1.0; 
-    DynamicalSystem sys(3, 0., 50., x0); 
+    VectorXd x0(2); x0 << 2, 0; 
+    DynamicalSystem sys(2, 0., 20., x0); 
     sys.set_dynamics(dyn);
 
-    int N = 5000;
+    int N = 500;
     ODEInt odeint(&sys, RK4, N);
 
     Trajectory T = odeint.integrate();
     cout << T << endl;
 
-    vector<double> XX, YY, ZZ; 
+    vector<double> XX, YY; 
     MatrixXd res = T.get_data();
     for (int i = 0; i < N; i++) {
         XX.push_back(res(i, 0)); 
         YY.push_back(res(i, 1)); 
-        ZZ.push_back(res(i, 2)); 
     }
     
-    plt::scatter(XX, YY, ZZ);
-    plt::title("Integration Lorentz system");
+    plt::plot(XX, YY);
+    plt::title("Solution of van der Pol Equation (\mu = 1) with ODE45");
     plt::show();
 
     return 0;
