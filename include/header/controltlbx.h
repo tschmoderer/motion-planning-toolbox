156d0a6932a6784967025771ec84eb16ea88326c
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-11-09 14:16:54 +0100

    update controltlbx main file

--- include/header/controltlbx.h
+++ include/header/controltlbx.h
@@ -2,7 +2,7 @@
 * @file controltlbx.h
 * @author T. Schmoderer (iathena@mailo.com)
 * @version 0.0.3
-* @date 2022-10-21
+* @date 2022-11-09
 * @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
 */
 /**
@@ -13,22 +13,25 @@
 #define CNTRLTLBX_H
 
 #include "utils.h"
-#include "controller/controls.h"
-#include "systems/controlSystem.h"
-#include "path/path.h"
+#include "odeint/odeint.h"
+#include "systems/dynamicalSystem.h"
+// #include "controller/controls.h"
+//#include "systems/controlSystem.h"
+//#include "path/path.h"
 #include "common/outputHelper.h"
+//#include "motionplanner/motionPlanner.h"
 
 /* VERSION MACROS */
 #define VERSION_MPP_TLBX "0.0.3"
 #define AUTHOR_MPP_TLBX "T. Schmoderer"
 #define CONTACT_MPP_TLBX "iathena@mailo.com"
 #define DOCUMENTATION_MPP_TLBX "https://tschmoderer.github.io/motion-planning-toolbox/html/index.html"
-#define CODE_MPP_TLBX "https://github.com/tschmoderer/motion-planning-toolbox"
+#define CODE_MPP_TLBX "https://github.com/tschmoderer/motion-planning-toolbox" 
 
 /**
-* @brief 
-* 
-*/
+ * @brief Welcome function 
+ * 
+ */
 void hello_world_control_tlbx() {
     std::cout << "Welcome to Motion Planning Toolbox" << std::endl;
     std::cout << "Version       : " << VERSION_MPP_TLBX << std::endl;
--- include/header/utils.h
+++ include/header/utils.h
@@ -2,7 +2,7 @@
 * @file utils.h
 * @author T. Schmoderer (iathena@mailo.com)
 * @version 0.0.3
-* @date 2022-10-19
+* @date 2022-11-07
 * @copyright Copyright (c) 2022. All rights reserved. This project is released under the GNU GENERAL PUBLIC LICENSE.
 */
 /**
@@ -18,5 +18,7 @@
 #include <cassert>
 #include <cstdint>
 #include <Eigen/Dense>
+
+
 
 #endif
