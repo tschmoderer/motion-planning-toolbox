60d314878806755f86e9138f0085f954d80d0b0c
Schmoderer <timothee.schmoderer@netcourrier.com>
2022-12-06 16:56:51 +0100

    update main application

---
+++ app/app.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, argparse, logging
+
+from assets.process import experience, loadData
+from assets.utils import log
+
+from assets.config import BUILD_DIR, BIN_DIR
+from assets.gui.gui import AppGui 
+
+logger   = log.setup_custom_logger('main', logging.DEBUG)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def cli(confFile): 
+    ## Load configuration file as dict data
+    logger.debug("CLI Mode")
+    logger.debug("Argument of python script is " + str(confFile))
+    if not os.path.isfile(confFile):
+        logger.critical("The provided argument is not a file or cannot be located.")
+        raise Exception("The provided argument is not a file or cannot be located.")
+
+    logger.info("The configuration file " + str(confFile) + " will be loaded.")
+    data = loadData.load_json_data(confFile)
+
+    ## Parse experiment data
+    exps = []
+    k    = 0
+    for d in data["experiences"]: 
+        k = k + 1
+        logger.info("Load setting for experience # " + str(k))
+        exps.append(experience.Experience(d))
+
+    ## Run experiments
+    k = 0
+    for exp in exps: 
+        k = k + 1
+        logger.info("Print Header file for exp # " + str(k) + " : " + exp.name)
+        exp.printHeader("bin/exp.hpp")
+
+        # Compile cpp program
+        logger.info("Compile for exp " + exp.name)
+        err = os.system("make -s -f " + BUILD_DIR + "/app/bin/Makefile exp")
+        if err: 
+            logger.error("There was an error in the compilation of your experiment.")
+            quit()
+        logger.info("COMPILATION SUCCESSFUL")
+
+        # Run cpp program 
+        logger.info("Run exp  # " + str(k) + " : " + exp.name)
+        err = os.system(BIN_DIR + "exp")
+
+        logger.info("End exp " + exp.name)
+
+    logger.info("Starting Post processing")
+
+def gui(confFile = None): 
+    """
+    TODO: implement pass data to gui 
+    """
+    AppGui(basedir=dir_path, data = None)
+
+if __name__ == "__main__":
+    desc = "A python command Line Interface for the C++ control ToolBox"
+    parser = argparse.ArgumentParser(description = desc)
+    
+    parser.add_argument("-g", "--gui", help="GUI Mode", required=False, action='store_true')
+
+    iscli = not (("--gui" in sys.argv) or ("-g" in sys.argv))
+    parser.add_argument("-i", "--input", help="Input configuration file (required in CLI mode)", required=iscli)
+
+    args = parser.parse_args()
+
+    if args.gui: 
+        gui()
+    else: 
+        cli(args.input)
--- app/bin/exp.cpp
+++ app/bin/exp.cpp
@@ -2,98 +2,71 @@
 #include "exp.hpp"
 #include "../../include/header/controltlbx.h"
 
-#ifdef EXP_TYPE_DYNAMICAL 
-StateVector_t dyn(double t, StateVector_t x) {
-    StateVector_t dxdt(EXP_STATE_DIM);
-    dxdt = StateVector_t::Zero(EXP_STATE_DIM);
+StateVector dyn(Time_t t, StateVector x) {
+    StateVector dxdt(EXP_STATE_DIM);
+    dxdt = StateVector::Zero(EXP_STATE_DIM);
     
     EXP_DYNAMICS_F
     
     return dxdt;
 }
 
-MatrixXd dyn_dx(double t, StateVector_t x) {
-    MatrixXd dxdt_dx(EXP_STATE_DIM, EXP_STATE_DIM);
-    dxdt_dx = MatrixXd::Zero(EXP_STATE_DIM, EXP_STATE_DIM);
+#ifdef EXP_DYNAMICS_dFdX
+StateMatrix dyn_dx(Time_t t, StateVector x) {
+    StateMatrix dxdt_dx(EXP_STATE_DIM, EXP_STATE_DIM);
+    dxdt_dx = StateMatrix::Zero(EXP_STATE_DIM, EXP_STATE_DIM);
     
     EXP_DYNAMICS_dFdX
 
     return dxdt_dx;
 }
-#else 
-#ifdef EXP_TYPE_CONTROL
-StateVector_t dyn(double t, StateVector_t x, Controls & u) {
-    StateVector_t dxdt(EXP_STATE_DIM);
-    VectorXd cntr(EXP_NB_CNTRL);
-    cntrl = u(t); 
-
-    EXP_DYNAMICS_F
-    
-    return dxdt;
-}
-
-MatrixXd dyn_dx(double t, StateVector_t x, Controls & u) {
-    MatrixXd dxdt_dx(EXP_STATE_DIM, EXP_STATE_DIM);
-    dxdt_dx = MatrixXd::Zero(EXP_STATE_DIM, EXP_STATE_DIM);
-    VectorXd cntr(EXP_NB_CNTRL);
-    cntrl = u(t); 
-
-    EXP_DYNAMICS_dFdX
-
-    return dxdt_dx;
-}
-
-MatrixXd dyn_du(double t, StateVector_t x, Controls & u) {
-    MatrixXd dxdt_du(EXP_STATE_DIM, EXP_NB_CNTRL);
-    dxdt_du = MatrixXd::Zero(EXP_STATE_DIM, EXP_NB_CNTRL);
-    VectorXd cntr(EXP_NB_CNTRL);
-    cntrl = u(t); 
-
-    EXP_DYNAMICS_dFdU
-
-    return dxdt_du;
-}
-
-#endif
 #endif
 
 int main(int argc, char **argv) {
     hello_world_control_tlbx();
 
-    StateVector_t x0s(EXP_NB_X0*EXP_STATE_DIM); x0s << EXP_X0S;
+    StateVector x0s(EXP_NB_X0*EXP_STATE_DIM); x0s << EXP_X0S;
 
-    StateVector_t x0(EXP_STATE_DIM); 
-    #ifdef EXP_TYPE_DYNAMICAL 
-        DynamicalSystem sys(EXP_STATE_DIM, EXP_START_TIME, EXP_END_TIME, x0, dyn, dyn_dx);
-    #else 
-        #ifdef EXP_TYPE_CONTROL
-        ControlSystem sys(EXP_STATE_DIM, EXP_NB_CNTRL, 
-                            EXP_START_TIME, EXP_END_TIME, 
-                            x0, 
-                            dyn, dyn_dx, dyn_du
-                        );
-        #endif
+    StateVector x0(EXP_STATE_DIM); 
+    TimeVector tspan; 
+
+    #ifdef EXP_TIMESTEPS_METHODS_TRAJ_LINSPACE
+        tspan =  TimeVector::linspace(EXP_START_TIME, EXP_END_TIME, EXP_DISCRETISATION_TRAJ);
+    #elif defined EXP_TIMESTEPS_METHODS_TRAJ_DATA
+        Time_t * val = new Time_t[EXP_DISCRETISATION_TRAJ]{EXP_TIMESTEPS_DATA}; 
+        tspan = TimeVector(EXP_DISCRETISATION_TRAJ, val);
+        delete [] val;
     #endif
-    ODEInt odeint(&sys, EXP_METHODS_ODE_INT, EXP_DISCRETISATION_TRAJ);
+
+    #ifdef EXP_DYNAMICS_dFdX
+    DynamicalSystem sys(EXP_STATE_DIM, EXP_START_TIME, EXP_END_TIME, x0, dyn, dyn_dx);
+    #else 
+    DynamicalSystem sys(EXP_STATE_DIM, EXP_START_TIME, EXP_END_TIME, x0, dyn);
+    #endif
+
+    ODEInt odeint(&sys, EXP_METHODS_ODE_INT, tspan);
     Trajectory traj;
 
     for (int i = 0; i < EXP_NB_X0; i++) {
+        cout << "Start : initial condition " << i+1 << " / " <<  EXP_NB_X0 << endl;
         x0 << x0s(Eigen::seq(i*EXP_STATE_DIM, (i+1)*EXP_STATE_DIM-1));
         sys.set_x0(x0);
         
         traj = odeint.integrate();
 
-        if (EXP_OUTPUT_TRAJ_CLI) {
-            cout << "Initial condition " << i+1 << " / " <<  EXP_NB_X0 << endl;
-            cout << traj << endl << endl;
-        }
+        OutputHelper oh_traj(traj);   
 
-        if (EXP_OUTPUT_TRAJ_FILE) {
-            string current_dir = argv[0];
-            current_dir = current_dir.substr(0, current_dir.find_last_of('/'));
-            string dir = current_dir + "/../" + EXP_OUTPUT_TRAJ_FILE_DIR + EXP_NAME + "/" + EXP_OUTPUT_TRAJ_FILE_SUBDIR ;
-            OutputHelper(traj, dir + EXP_OUTPUT_TRAJ_FILE_FNAME, dir + EXP_OUTPUT_TRAJ_FILE_TIME_FNAME).format(CSVFormat).save(i+1);
-        }
+        #if EXP_OUTPUT_CLI
+            cout << oh_traj << endl << endl;
+        #endif
+
+        #if EXP_OUTPUT_FILE
+            string dir = SOURCE_DIR + string("/") + EXP_OUTPUT_DIR + EXP_NAME + string("/");
+            oh_traj.set_out_directory(dir); 
+            oh_traj.format(CSVFormat).save(i+1);
+        #endif
+        
+        cout << "End   : initial condition " << i+1 << " / " <<  EXP_NB_X0 << endl;
     }
 
     return 0;   
